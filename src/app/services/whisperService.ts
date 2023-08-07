522f9325e758f0608af22c7c7918b90682b80296
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-08-07 19:11:51 +0300

    remove unused whisper method

--- src/app/services/whisperService.ts
+++ src/app/services/whisperService.ts
@@ -1,17 +1,4 @@
 import { prisma } from "../../../prisma/client";
-import { getUserIdByMail } from "./userService";
-
-export const addWhisper = async (text: string, email: string) => {
-  const user = await getUserIdByMail(email);
-  if (user) {
-    await prisma.whisper.create({
-      data: {
-        text: text,
-        userId: user.id,
-      },
-    });
-  }
-};
 
 export const getAllWhispers = async () => {
   return await prisma.whisper.findMany({
